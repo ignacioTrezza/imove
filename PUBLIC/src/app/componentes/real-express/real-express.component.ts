4b25888f6da3779683591e86455a52960c22cff0
ignacioTrezza <ignacio.trezza@gmail.com>
2024-03-25 03:13:44 -0300

    feature/select Canvas currentMode

--- PUBLIC/src/app/componentes/real-express/real-express.component.ts
+++ PUBLIC/src/app/componentes/real-express/real-express.component.ts
@@ -4,7 +4,6 @@ import { WebsocketService } from '../../services/web-socket.service';
 import * as THREE from 'three';
 import { Store, select } from '@ngrx/store';
 import { AppState } from '../../core/store/app.state';
-import { selectToggleClientEventHandling, selectToggleRemoteClick } from '../../core/store/selectors/app.selectors';
 import { ElectronService } from '../../services/electron.service';
 import * as AppSelectors from '../../core/store/selectors/app.selectors';
 @Component({
@@ -14,6 +13,9 @@ import * as AppSelectors from '../../core/store/selectors/app.selectors';
   styleUrls: ['./real-express.component.scss']
 })
 export class RealExpressComponent implements OnInit, OnDestroy, AfterViewInit {
+
+  currentMode: 'cubeRotation' | 'cubePosition' | 'sceneRotation' | 'scenePosition' | 'cameraRotation' | 'cameraPosition' | null = null;
+
 
   toggleRemoteClickk!: boolean;
   toggleEventHandlingg!: boolean;
@@ -101,7 +103,7 @@ export class RealExpressComponent implements OnInit, OnDestroy, AfterViewInit {
         this.toggleClientEventHandlingg = toggleClientEventHandling;
       });
 
-      if (this.toggleClientEventHandlingg) {
+      if (this.toggleRemoteClickk) {
         this.handleClientEvent();
       } else {
         document.removeEventListener('keydown', this.handleKeyDown.bind(this));
@@ -111,114 +113,115 @@ export class RealExpressComponent implements OnInit, OnDestroy, AfterViewInit {
       }
       if (this.toggleEventHandlingg) {
         let parsedData = { x: 0, y: 0, z: 0 };
-     
-          if (this.toggleAccelerometerr) {
-            parsedData = { ...parsedData, x: this.accelX, y: this.accelY, z: this.accelZ }
-          }
-          if (this.toggleAccelerometerIncludingGravityy) {
-            parsedData = { ...parsedData, x: this.accelIncludingGravityX, y: this.accelIncludingGravityY, z: this.accelIncludingGravityZ }
-          }
-          if (this.toggleGyroscopee) {
-            parsedData = { ...parsedData, x: this.gyroAlpha, y: this.gyroBeta, z: this.gyroGamma }
-          }
-          console.log(this.cube.position.x, this.cube.position.y, this.cube.position.z);
-
-          if (parsedData.x !== null && parsedData.y !== null && parsedData.z !== null) {
-
-            const deltaX = parsedData.x - this.previousX;
-            const deltaY = parsedData.y - this.previousY;
-            this.scene.rotation.y += deltaX * 0.005;
-            this.scene.rotation.x += deltaY * 0.005;
-            this.previousX = parsedData.x;
-            this.previousY = parsedData.y;
-          } else {
-            console.log('XYZ:', parsedData.x, parsedData.y, parsedData.z, this.cube.position.x, this.cube.position.y, this.cube.position.z);
-          }
-        
-      }
 
         if (this.toggleAccelerometerr) {
-          this.accelSubscription = this.websocketService.accelerometerData.subscribe(({ x, y, z }) => {
-            console.log(`Accelerometer: x=${x}, y=${y}, z=${z}`);
-            this.accelX = x;
-            this.accelY = y;
-            this.accelZ = z;
-            // Handle accelerometer data
-          });
-        } else {
-          this.accelSubscription.unsubscribe();
+          parsedData = { ...parsedData, x: this.accelX, y: this.accelY, z: this.accelZ }
         }
-
-        // if (this.toggleMousePoss) {
-        //   this.processedPointerSubscription = this.websocketService.processedPointerData.subscribe(({ x, y, z }) => {
-        //     console.log(`Processed Pointer: x=${x}, y=${y}, z=${z}`);
-        //     this.newX = x;
-        //     this.newY = y;
-        //     this.newZ = z;
-        //     // Handle processed pointer data
-        //   });
-        // } else {
-        //   this.processedPointerSubscription.unsubscribe();
-        // }
-
-        if (this.toggleGyroscopee) {
-          this.gyroSubscription = this.websocketService.gyroscopeData.subscribe(({ alpha, beta, gamma }) => {
-            console.log(`Gyroscope: alpha=${alpha}, beta=${beta}, gamma=${gamma}`);
-            this.gyroAlpha = alpha;
-            this.gyroBeta = beta;
-            this.gyroGamma = gamma;
-            // Handle gyroscope data
-          });
-        } else {
-          this.gyroSubscription.unsubscribe();
-        }
-
         if (this.toggleAccelerometerIncludingGravityy) {
-          this.accelIncludingGravitySubscription = this.websocketService.accelerometerIncludingGravityData
-            .subscribe(({ x, y, z }) => {
-              console.log(`Accelerometer Including Gravity: x=${x}, y=${y}, z=${z}`);
-              this.accelIncludingGravityX = x;
-              this.accelIncludingGravityY = y;
-              this.accelIncludingGravityZ = z;
-            })
-        } else {
-          this.accelIncludingGravitySubscription.unsubscribe();
+          parsedData = { ...parsedData, x: this.accelIncludingGravityX, y: this.accelIncludingGravityY, z: this.accelIncludingGravityZ }
+        }
+        if (this.toggleGyroscopee) {
+          parsedData = { ...parsedData, x: this.gyroAlpha, y: this.gyroBeta, z: this.gyroGamma }
+        }
+        console.log(this.cube.position.x, this.cube.position.y, this.cube.position.z);
+
+        if (parsedData.x !== null && parsedData.y !== null && parsedData.z !== null) {
+
+          const deltaX = parsedData.x - this.previousX;
+          const deltaY = parsedData.y - this.previousY;
+  
+          this.switchMode(this.currentMode, deltaX, deltaY);
+           
+          this.previousX = parsedData.x;
+          this.previousY = parsedData.y;
+  
+        }else {
+          console.log('XYZ:', parsedData.x, parsedData.y, parsedData.z, this.cube.position.x, this.cube.position.y, this.cube.position.z);
         }
       }
+    
+      if (this.toggleAccelerometerr) {
+        this.accelSubscription = this.websocketService.accelerometerData.subscribe(({ x, y, z }) => {
+          console.log(`Accelerometer: x=${x}, y=${y}, z=${z}`);
+          this.accelX = x;
+          this.accelY = y;
+          this.accelZ = z;
+          // Handle accelerometer data
+        });
+      } else {
+        this.accelSubscription.unsubscribe();
+      }
+
+      // if (this.toggleMousePoss) {
+      //   this.processedPointerSubscription = this.websocketService.processedPointerData.subscribe(({ x, y, z }) => {
+      //     console.log(`Processed Pointer: x=${x}, y=${y}, z=${z}`);
+      //     this.newX = x;
+      //     this.newY = y;
+      //     this.newZ = z;
+      //     // Handle processed pointer data
+      //   });
+      // } else {
+      //   this.processedPointerSubscription.unsubscribe();
+      // }
+
+      if (this.toggleGyroscopee) {
+        this.gyroSubscription = this.websocketService.gyroscopeData.subscribe(({ alpha, beta, gamma }) => {
+          console.log(`Gyroscope: alpha=${alpha}, beta=${beta}, gamma=${gamma}`);
+          this.gyroAlpha = alpha;
+          this.gyroBeta = beta;
+          this.gyroGamma = gamma;
+          // Handle gyroscope data
+        });
+      } else {
+        this.gyroSubscription.unsubscribe();
+      }
+
+      if (this.toggleAccelerometerIncludingGravityy) {
+        this.accelIncludingGravitySubscription = this.websocketService.accelerometerIncludingGravityData
+          .subscribe(({ x, y, z }) => {
+            console.log(`Accelerometer Including Gravity: x=${x}, y=${y}, z=${z}`);
+            this.accelIncludingGravityX = x;
+            this.accelIncludingGravityY = y;
+            this.accelIncludingGravityZ = z;
+          })
+      } else {
+        this.accelIncludingGravitySubscription.unsubscribe();
+      }
+    }
 
 
-      this.websocketService.handleClientEvent.subscribe((value) => {
-        if (value) {
-          if (this.toggleClientEventHandlingg === true) {
-            this.handleClientEvent();
-          } else {
-            document.removeEventListener('keydown', this.handleKeyDown.bind(this));
-            document.removeEventListener('mouseup', this.mouseUpListener);
-            document.removeEventListener('mousedown', this.mouseUpListener);
-            document.removeEventListener('mousemove', this.mouseUpListener);
-          }
+    this.websocketService.handleClientEvent.subscribe((value) => {
+      if (value) {
+        if (this.toggleRemoteClickk === true) {
+          this.handleClientEvent();
+        } else {
+          document.removeEventListener('keydown', this.handleKeyDown.bind(this));
+          document.removeEventListener('mouseup', this.mouseUpListener);
+          document.removeEventListener('mousedown', this.mouseUpListener);
+          document.removeEventListener('mousemove', this.mouseUpListener);
         }
-      });
-    }
-    moveCursorBasedOnAcceleration(currentX: number, currentY: number, x: number, y: number, z: number): void {
-      if(
-        this.electronService.isElectron) {
-      // Normalize acceleration data to a range that suits your screen size and preferences
-      const sensitivity = 10; // Adjust this based on your needs
-      let deltaX = x * sensitivity;
-      let deltaY = y * sensitivity;
+      }
+    });
+  }
+  // moveCursorBasedOnAcceleration(currentX: number, currentY: number, x: number, y: number, z: number): void {
+  //   if (
+  //     this.electronService.isElectron) {
+  //     // Normalize acceleration data to a range that suits your screen size and preferences
+  //     const sensitivity = 10; // Adjust this based on your needs
+  //     let deltaX = x * sensitivity;
+  //     let deltaY = y * sensitivity;
 
-      // Calculate new position
-      this.newX = currentX + deltaX;
-      this.newY = currentY - deltaY; // Subtract deltaY because screen coordinates in Y are inverted
-      this.newZ = 0;
-      // Boundary checks (assuming screen resolution of 1920x1080, adjust as needed)
-      this.newX = Math.max(0, Math.min(this.newX, 1920));
-      this.newY = Math.max(0, Math.min(this.newY, 1080));
-      // Handle accelerometer including gravity data
-      this.websocketService.emitProcessedPointerData(this.newX, this.newY, this.newZ);
-    }
-  };
+  //     // Calculate new position
+  //     this.newX = currentX + deltaX;
+  //     this.newY = currentY - deltaY; // Subtract deltaY because screen coordinates in Y are inverted
+  //     this.newZ = 0;
+  //     // Boundary checks (assuming screen resolution of 1920x1080, adjust as needed)
+  //     this.newX = Math.max(0, Math.min(this.newX, 1920));
+  //     this.newY = Math.max(0, Math.min(this.newY, 1080));
+  //     // Handle accelerometer including gravity data
+  //     this.websocketService.emitProcessedPointerData(this.newX, this.newY, this.newZ);
+  //   }
+  // };
 
   ngOnDestroy(): void {
     document.removeEventListener('keydown', this.handleKeyDown.bind(this));
@@ -264,9 +267,9 @@ export class RealExpressComponent implements OnInit, OnDestroy, AfterViewInit {
     }
     this.scene.add(this.cube);
     this.camera.position.z = 100;
-    const axisX = new THREE.Mesh(new THREE.BoxGeometry(10, 0.1, 0.1), new THREE.MeshBasicMaterial({ color: 0xffffff }));
-    const axisY = new THREE.Mesh(new THREE.BoxGeometry(0.1, 10, 0.1), new THREE.MeshBasicMaterial({ color: 0xffffff }));
-    const axisZ = new THREE.Mesh(new THREE.BoxGeometry(0.1, 0.1, 10), new THREE.MeshBasicMaterial({ color: 0xffffff }));
+    const axisX = new THREE.Mesh(new THREE.BoxGeometry(10, 0.1, 0.1), new THREE.MeshBasicMaterial({ color: 0xffaafff }));
+    const axisY = new THREE.Mesh(new THREE.BoxGeometry(0.1, 10, 0.1), new THREE.MeshBasicMaterial({ color: 0xffbbff }));
+    const axisZ = new THREE.Mesh(new THREE.BoxGeometry(0.1, 0.1, 10), new THREE.MeshBasicMaterial({ color: 0xffccff }));
 
     axisX.position.set(5, 0, 0);
     axisY.position.set(0, 5, 0);
@@ -299,19 +302,55 @@ export class RealExpressComponent implements OnInit, OnDestroy, AfterViewInit {
       if (this.isDragging) {
         const deltaX = event.clientX - this.previousX;
         const deltaY = event.clientY - this.previousY;
-        if (event.buttons === 1) {
-          this.scene.rotation.y += deltaX * 0.005;
-          this.scene.rotation.x += deltaY * 0.005;
-        } else if (event.buttons === 2) {
-          this.camera.position.x += deltaX * 0.01;
-          this.camera.position.y -= deltaY * 0.01;
-        }
+
+       
+        this.switchMode(this.currentMode, deltaX, deltaY);
+
         this.previousX = event.clientX;
         this.previousY = event.clientY;
+
       }
     });
     document.getElementById('threejs-container')!.addEventListener('contextmenu', (event) => {
       event.preventDefault();
     });
+  }
+  switchMode(currentMode: any, deltaX: number, deltaY: number){
+    switch (currentMode) {
+    case 'cubeRotation':
+      this.cube.rotation.x += deltaX * 0.005;
+      this.cube.rotation.y += deltaY * 0.005;
+      break;
+    case 'cubePosition':
+      this.cube.position.x += deltaX * 0.01;
+      this.cube.position.y -= deltaY * 0.01;
+      break;
+    case 'sceneRotation':
+      this.scene.rotation.y += deltaX * 0.005;
+      this.scene.rotation.x += deltaY * 0.005;
+      break;
+    case 'scenePosition':
+      this.scene.position.x += deltaX * 0.01;
+      this.scene.position.y += deltaY * -0.01;
+    break;
+    case 'cameraRotation':
+      this.camera.rotation.x += deltaX * -0.01;
+      this.camera.rotation.y -= deltaY * -0.01;
+      break;
+      
+    case 'cameraPosition':
+      this.camera.position.x += deltaX * 0.01;
+      this.camera.position.y -= deltaY * 0.01;
+      break;
+    default:
+      console.log('No mode selected or unsupported mode');
+  }
+
+  return 
+ 
+}
+  setMode(mode: 'cubeRotation' | 'cubePosition' | 'sceneRotation' | 'scenePosition' | 'cameraRotation' | 'cameraPosition'): void {
+    this.currentMode = mode;
+    console.log(`Mode set to: ${mode}`);
   }
 }
